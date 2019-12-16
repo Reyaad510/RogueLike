8169c5d1af317a2db107d49dc06de7233cebf9f1
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-16 14:20:40 -0500

    Added player moveSpeed

---
+++ Roguelike/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3f;
+    private Vector2 moveInput;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y  = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
+
+        
+
+
+    }
+}
