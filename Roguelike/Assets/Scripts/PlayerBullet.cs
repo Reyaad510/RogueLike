bc7556c8dab01565c565dfa82128de1302144e28
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-16 21:33:38 -0500

    Player bullet moves to the right and disappears when hit wall

---
+++ Roguelike/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+
+    [SerializeField] float bulletSpeed = 7.5f;
+
+    // cached
+    Rigidbody2D bulletRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletRigidBody.velocity = transform.right * bulletSpeed;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+
+
+
+
+}
