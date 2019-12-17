ea0cc2d37f4d5aee991c9e4d9985d88d8ca50219
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-16 20:33:23 -0500

    Added rotation for gun in accordance with mouse

--- Roguelike/Assets/Scripts/PlayerController.cs
+++ Roguelike/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     // Config
     [SerializeField] float moveSpeed = 6f;
+    [SerializeField] Transform gunArm;
     private Vector2 moveInput;
+
 
     // cached component references
     Rigidbody2D myRigidBody;
@@ -23,6 +25,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Run();
+        WeaponPosition();
     }
 
     private void Run()
@@ -32,9 +35,17 @@ public class PlayerController : MonoBehaviour
 
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
         myRigidBody.velocity = playerVelocity;
+    }
 
-        
+    private void WeaponPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
 
-
+        // rotate gun hand
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angleOfWeaponAndMouse = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        // Quaternion.Euler() converts Rotation into a vector3. In Unity if you debug you will see rotation has other values and vector3 alone wouldnt work so have to do it like this
+        gunArm.rotation = Quaternion.Euler(0, 0, angleOfWeaponAndMouse);
     }
 }
