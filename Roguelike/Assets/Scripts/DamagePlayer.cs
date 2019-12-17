5cf654429cfbcd35bfec451927270ed56b263834
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-17 17:13:30 -0500

    Created spikes and prefab into Level Objects

---
+++ Roguelike/Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // When player hits object that damages player will trigger damage them
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+        
+    }
+
+
+    // if player stays on object that damages they will take damage
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+
+    }
+
+
+
+
+    // Collision used if you dont want player to run on them. Would count as a solid object. Change "IsTrigger" to uncheck
+    // When player hits object that damages player will damage them. Uses collider
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+
+    }
+
+
+    // if player stays on object that damages they will take damage
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+
+    }
+
+
+
+}
