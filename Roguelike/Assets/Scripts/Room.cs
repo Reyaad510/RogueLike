b580fa6cb393304628105d82821dc3e3f687db78
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-18 17:26:07 -0500

    Added room logic for player to kill all enemies in room for doors to open

--- Roguelike/Assets/Scripts/Room.cs
+++ Roguelike/Assets/Scripts/Room.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    // Says if doors close when player enter a room
     public bool doorCloseWhenEnter;
+    // Says if a specific room has this specific clear condition
+    public bool openDoorsWhenEnemiesCleared;
+
+    // Says if player is in that specific room
+    private bool _roomActive;
     public GameObject[] doors;
 
+    // Lists are easy to change the size of an array
+    public List<GameObject> enemies = new List<GameObject>();
 
 
+    private void Update()
+    {
+        // Loop through room and check enemy at each indexes
+        // When enemy killed will be removed 
+        if(enemies.Count > 0 && _roomActive && openDoorsWhenEnemiesCleared)
+        {
+            for(var i = 0; i < enemies.Count; i++)
+            {
+                if(enemies[i] == null)
+                {
+                    // i-- to prevent error
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
+            
+            if(enemies.Count == 0)
+            {
+                foreach(GameObject door in doors)
+                {
+                    door.SetActive(false);
+                    doorCloseWhenEnter = false;
+                }
+            }
+        }
+    }
 
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -23,6 +57,20 @@ public class Room : MonoBehaviour
                     door.SetActive(true);
                 }
             }
+
+            _roomActive = true;
+        }
+    }
+
+
+    // If player left room then room isnt active. 
+    // We do this to prevent the Update of counting enemies for every single room at any point in time
+    // Only want to happen for the room that the player is currently in
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            _roomActive = false;
         }
     }
 }
