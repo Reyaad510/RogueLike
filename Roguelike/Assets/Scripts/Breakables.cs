03e6abb7192e0a6d96d0fc2af4de9be705e87790
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-17 20:46:59 -0500

    Boxes able to randomly drop an item with a random chance

--- Roguelike/Assets/Scripts/Breakables.cs
+++ Roguelike/Assets/Scripts/Breakables.cs
@@ -8,6 +8,11 @@ public class Breakables : MonoBehaviour
     [SerializeField] int maxParts = 5;
     [SerializeField] int minParts = 2;
 
+    [Header("Items")]
+    public bool shouldDropItem;
+    [SerializeField] GameObject[] itemsToDrop;
+    [SerializeField] float itemDropPercent;
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,12 +37,29 @@ public class Breakables : MonoBehaviour
 
 
                 // Loop through random amount to determine when break box will show a certain amount of broken box parts
+                // Show broken parts
                 int partsToDrop = Random.Range(minParts, maxParts);
               
                 for(int i = 0; i < partsToDrop; i++)
                 {
                     int randomBrokenPart = Random.Range(0, brokenPart.Length);
                     Instantiate(brokenPart[randomBrokenPart], transform.position, transform.rotation);
+                }
+
+                // drop items
+                if (shouldDropItem)
+                {
+                    // float random range INCLUDES abilitiy to get top number. If were int it would NOT
+                    float dropChance = Random.Range(0f, 100f);
+
+                    // will drop item
+                    if(dropChance < itemDropPercent)
+                    {
+                        // giving list of items to randomly drop
+                        int randomItem = Random.Range(0, itemsToDrop.Length);
+                        Instantiate(itemsToDrop[randomItem], transform.position, transform.rotation);
+                    }
+
                 }
 
                 
--- Roguelike/Assets/Scripts/HealthPickup.cs
+++ Roguelike/Assets/Scripts/HealthPickup.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HealthPickup : MonoBehaviour
 {
     [SerializeField] int healAmount = 1;
+    [SerializeField] float waitToBeCollected = 0.5f;
 
 
     // Start is called before the first frame update
@@ -16,15 +17,25 @@ public class HealthPickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        WaitToBeCollected();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && waitToBeCollected <= 0)
         {
             PlayerHealthController.instance.HealPlayer(healAmount);
             Destroy(gameObject);
+        }
+    }
+
+
+    // Fixes issue where player would IMMEDIATELY pickup item after breaking boxes. Allows time to not pick it up
+    void WaitToBeCollected()
+    {
+        if (waitToBeCollected > 0)
+        {
+            waitToBeCollected -= Time.deltaTime;
         }
     }
 
