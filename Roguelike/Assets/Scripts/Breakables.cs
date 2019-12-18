9e88ea072057476c5a2f54fe9933b9dfdaf20bd2
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-18 15:15:51 -0500

    Fixed Object interactions using Layers and tags

--- Roguelike/Assets/Scripts/Breakables.cs
+++ Roguelike/Assets/Scripts/Breakables.cs
@@ -28,6 +28,7 @@ public class Breakables : MonoBehaviour
         
     }
 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
@@ -35,39 +36,61 @@ public class Breakables : MonoBehaviour
             // Says if we are dashing then break the breakable
             if (PlayerController.instance.dashCounter > 0)
             {
-                Destroy(gameObject);
-                AudioManager.instance.PlaySFX(breakSound);
 
-                // Loop through random amount to determine when break box will show a certain amount of broken box parts
-                // Show broken parts
-                int partsToDrop = Random.Range(minParts, maxParts);
-              
-                for(int i = 0; i < partsToDrop; i++)
-                {
-                    int randomBrokenPart = Random.Range(0, brokenPart.Length);
-                    Instantiate(brokenPart[randomBrokenPart], transform.position, transform.rotation);
-                }
+                BreakBreakables();
 
-                // drop items
-                if (shouldDropItem)
-                {
-                    // float random range INCLUDES abilitiy to get top number. If were int it would NOT
-                    float dropChance = Random.Range(0f, 100f);
-
-                    // will drop item
-                    if(dropChance < itemDropPercent)
-                    {
-                        // giving list of items to randomly drop
-                        int randomItem = Random.Range(0, itemsToDrop.Length);
-                        Instantiate(itemsToDrop[randomItem], transform.position, transform.rotation);
-                    }
-
-                }
-
-                
             }
         }
+
+        if (other.tag == "Player Bullet")
+        {
+            BreakBreakables();
+        }
     }
+
+
+
+    public void BreakBreakables()
+    {
+        Destroy(gameObject);
+        AudioManager.instance.PlaySFX(breakSound);
+
+        BreakableItemDropChance();
+
+
+    }
+
+
+    void BreakableItemDropChance()
+    {
+        // Loop through random amount to determine when break box will show a certain amount of broken box parts
+        // Show broken parts
+        int partsToDrop = Random.Range(minParts, maxParts);
+
+        for (int i = 0; i < partsToDrop; i++)
+        {
+            int randomBrokenPart = Random.Range(0, brokenPart.Length);
+            Instantiate(brokenPart[randomBrokenPart], transform.position, transform.rotation);
+        }
+
+        // drop items
+        if (shouldDropItem)
+        {
+            // float random range INCLUDES abilitiy to get top number. If were int it would NOT
+            float dropChance = Random.Range(0f, 100f);
+
+            // will drop item
+            if (dropChance < itemDropPercent)
+            {
+                // giving list of items to randomly drop
+                int randomItem = Random.Range(0, itemsToDrop.Length);
+                Instantiate(itemsToDrop[randomItem], transform.position, transform.rotation);
+            }
+
+        }
+    }
+
+
 
 
 }
