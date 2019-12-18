0217800b6001ee9ee883bec8905fc92cd328b604
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-18 16:20:53 -0500

    Setup camera script

---
+++ Roguelike/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+    [Header("Numbers")]
+    public float moveSpeed;
+
+    [Header("Objects")]
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), moveSpeed * Time.deltaTime);
+        }
+    }
+
+
+
+}
