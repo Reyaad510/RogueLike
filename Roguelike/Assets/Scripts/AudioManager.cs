f8437ced74c1ede9bb9e216c8b1244d6e4469b22
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-17 21:28:16 -0500

    Added music in

---
+++ Roguelike/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance;
+    public AudioSource levelMusic, gameOverMusic, winMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGameOver()
+    {
+        levelMusic.Stop();
+        gameOverMusic.Play();
+    }
+
+    public void PlayWinMusic()
+    {
+        levelMusic.Stop();
+        winMusic.Play();
+    }
+
+}
