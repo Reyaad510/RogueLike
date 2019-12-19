94de62c247aef10874719e2d651b098dc2e722a1
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-19 18:27:38 -0500

    Able to generate one room top, left, right, or bottom of main room

--- Roguelike/Assets/Scripts/LevelGenerator.cs
+++ Roguelike/Assets/Scripts/LevelGenerator.cs
@@ -6,30 +6,68 @@ public class LevelGenerator : MonoBehaviour
 {
 
     [SerializeField] int distanceToEnd;
+    [SerializeField] float xOffSet = 17f;
+    [SerializeField] float yOffSet = 10f;
 
 
     [SerializeField] Color startColor, endColor;
     [SerializeField] GameObject layoutRoom;
     [SerializeField] Transform generatorPoint;
 
-
-
-    private void Awake()
-    {
- 
-    }
+    // creating a type of variable called Direction
+    // The names associated with numbers like an array(hover over them to see)
+    public enum Direction { up, right, down, left };
+    public Direction selectedDirection;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+        InstantiateNewRoom();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
+
+
+    private void InstantiateNewRoom()
+    {
+        Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+        // Casting number into direction
+        selectedDirection = (Direction)Random.Range(0, 4);
+        MoveGenerationPoint();
+    }
+
+
+
+    private void MoveGenerationPoint()
+    {
+        switch (selectedDirection)
+        {
+            case Direction.up:
+                generatorPoint.position += new Vector3(0f, yOffSet, 0f);
+                break;
+
+            case Direction.down:
+                generatorPoint.position += new Vector3(0f, -yOffSet, 0f);
+                break;
+
+            case Direction.right:
+                generatorPoint.position += new Vector3(xOffSet, 0, 0f);
+                break;
+
+            case Direction.left:
+                generatorPoint.position += new Vector3(-xOffSet, 0f, 0f);
+                break;
+
+
+        }
+    }
+
+
 }
