fb8c6fd06b20adcb5b1030f90f14679062d52fec
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-19 17:27:26 -0500

    Setting up code to procedurally generate rooms

---
+++ Roguelike/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    [SerializeField] int distanceToEnd;
+
+
+    [SerializeField] Color startColor, endColor;
+    [SerializeField] GameObject layoutRoom;
+    [SerializeField] Transform generatorPoint;
+
+
+
+    private void Awake()
+    {
+ 
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
