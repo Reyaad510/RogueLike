2c4f7a1978c53199699ca53ea581360dcbec033b
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-20 22:41:28 -0500

    Procedurally generating outlines for connecting rooms

--- Roguelike/Assets/Scripts/LevelGenerator.cs
+++ Roguelike/Assets/Scripts/LevelGenerator.cs
@@ -36,6 +36,8 @@ public class LevelGenerator : MonoBehaviour
 
     public RoomPrefabs rooms;
 
+    private List<GameObject> generatedOutlines = new List<GameObject>();
+
 
 
     // Start is called before the first frame update
@@ -50,6 +52,8 @@ public class LevelGenerator : MonoBehaviour
     {
         ReloadSceneForDebug();
     }
+
+
 
 
     private void InstantiateStartRoom()
@@ -94,7 +98,11 @@ public class LevelGenerator : MonoBehaviour
                 MoveGenerationPoint();
             }
         }
+
+        GetRoomPositionsForGeneratingOutlines();
+
     }
+
 
 
 
@@ -120,6 +128,130 @@ public class LevelGenerator : MonoBehaviour
 
         }
     
+    }
+
+
+    private void GetRoomPositionsForGeneratingOutlines()
+    {
+        // create room outline
+        CreateRoomOutline(Vector3.zero);
+        foreach (GameObject room in layoutRoomObjects)
+        {
+            CreateRoomOutline(room.transform.position);
+        }
+        CreateRoomOutline(endRoom.transform.position);
+    }
+
+
+
+    public void CreateRoomOutline(Vector3 roomPosition)
+    {
+
+        bool roomAbove = Physics2D.OverlapCircle(roomPosition + new Vector3(0f, yOffSet, 0f), 0.2f, whatIsRoom);
+        bool roomBelow = Physics2D.OverlapCircle(roomPosition + new Vector3(0f, -yOffSet, 0f), 0.2f, whatIsRoom);
+        bool roomLeft = Physics2D.OverlapCircle(roomPosition + new Vector3(-xOffSet, 0f, 0f), 0.2f, whatIsRoom);
+        bool roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffSet, 0f, 0f), 0.2f, whatIsRoom);
+
+        int directionCount = 0;
+
+        // When we make a new room we will check how many rooms are around it and add that to a counter
+        if (roomAbove)
+        {
+            directionCount++;
+        }
+        if (roomBelow)
+        {
+            directionCount++;
+        }
+        if (roomLeft)
+        {
+            directionCount++;
+        }
+        if (roomRight)
+        {
+            directionCount++;
+        }
+
+        switch (directionCount)
+        {
+            // depending on counter number will tell us how many rooms are near us
+            // We can then find right room amount to generate the correct outline
+            case 0:
+                Debug.Log("Found no room exists!");
+                break;
+
+            case 1:
+                if (roomAbove)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.singleUp, roomPosition, transform.rotation));
+                }
+                if (roomBelow)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.singleDown, roomPosition, transform.rotation));
+                }
+                if (roomLeft)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.singleLeft, roomPosition, transform.rotation));
+                }
+                if (roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.singleRight, roomPosition, transform.rotation));
+                }
+                break;
+            case 2:
+                if(roomAbove && roomBelow)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleUpDown, roomPosition, transform.rotation));
+                }
+                if (roomLeft && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleLeftRight, roomPosition, transform.rotation));
+                }
+                if (roomAbove && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleUpRight, roomPosition, transform.rotation));
+                }
+                if (roomRight && roomBelow)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleRightDown, roomPosition, transform.rotation));
+                }
+                if (roomBelow && roomLeft)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleDownLeft, roomPosition, transform.rotation));
+                }
+                if (roomLeft && roomAbove)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleLeftUp, roomPosition, transform.rotation));
+                }
+
+                break;
+            case 3:
+                if (roomAbove && roomRight && roomBelow)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.tripleUpRightDown, roomPosition, transform.rotation));
+                }
+                if (roomRight && roomBelow && roomLeft)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.tripleRightDownLeft, roomPosition, transform.rotation));
+                }
+                if (roomBelow && roomLeft && roomAbove)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.tripleDownLeftUp, roomPosition, transform.rotation));
+                }
+                if (roomLeft && roomAbove && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.tripleLeftUpRight, roomPosition, transform.rotation));
+                }
+
+                break;
+            case 4:
+                if (roomAbove && roomRight && roomBelow && roomLeft)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.fourway, roomPosition, transform.rotation));
+                }
+
+                break;
+        }
     }
 
 
